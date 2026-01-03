654ff522dd2fab70a761f010e8adc7e69c637779
Mohammad Sami <mohammadsami9555@gmail.com>
2026-01-03 23:04:01 +0530

    test preview deployment

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
 
         <p>
-          Hello Preview Deployment 👋
+          Preview Deployment Test 🚀
         </p>
 
         <a
